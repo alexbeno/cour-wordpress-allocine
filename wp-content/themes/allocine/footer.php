db81ebee4f4939c462a0557db07f7e8982dd147c
alexbeno <alexbenoliel@gmail.com>
2017-10-13 11:56:54 +0200

    git

---
+++ wp-content/themes/allocine/footer.php
@@ -0,0 +1,7 @@
+  <footer>
+
+  </footer>
+  <!-- Execution de la fonction wp_footer() obligatoire ! -->
+<?php wp_footer();  ?>
+  </body>
+</html>
---
+++ wp-content/themes/allocine/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <!-- Execution de la fonction wp_head() obligatoire ! -->
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+  <header>
+    <nav>
+    </nav>
+  </header>
